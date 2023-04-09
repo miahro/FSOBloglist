0ffbe551fc14d9bf36ddaed85062fda3e7a244df
miahro <mikko.ahro@gmail.com>
2023-04-09 15:05:46 +0300

    Ex 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,6 +5,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+
+const totalLikes = (blogs) => {
+  let total = blogs.reduce((sum,blog) => sum + blog.likes, 0)
+  logger.info(total)
+  return total
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
