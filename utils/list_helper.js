f8f755e084da13f62ac2ca50692c1987bbe5b0d4
miahro <mikko.ahro@gmail.com>
2023-04-10 18:03:02 +0300

    Ex 4.6-4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,9 @@
+const _ = require('lodash')
+
 const logger = require('./logger')
 
 const dummy = (blogs) => {
-  //logger.info (blogs)
+  logger.info (blogs)
   return 1
 }
 
@@ -23,8 +25,43 @@ const favoriteBlog = (blogs) => {
 
 }
 
+const mostBlogs = (blogs) => {
+  const blogsByAuthor = _.countBy(blogs, 'author')
+  // logger.info('blogsByAuthor', blogsByAuthor)
+  const maxAuthor = _.maxBy(_.keys(blogsByAuthor), (author) => blogsByAuthor[author])
+  // logger.info('maxAuthor', maxAuthor)
+  const result = {
+    'author': maxAuthor,
+    'blogs': blogsByAuthor[maxAuthor]
+  }
+  // logger.info(result)
+  return result
+}
+
+const mostLikes = (blogs) => {
+  const groupedByAuthor = _.groupBy(blogs, 'author')
+  // logger.info('groupedByAuthor: ', groupedByAuthor)
+  const sumOfLikesByAuthor = _.mapValues(groupedByAuthor, (blogs) => _.reduce(blogs, (sum, blog) => sum + blog.likes,0))
+  // logger.info('sumOfLikesbyAuthor', sumOfLikesByAuthor)
+  // logger.info('Chan: ', sumOfLikesByAuthor['Michael Chan'])
+  // logger.info('Martin: ', sumOfLikesByAuthor['Robert C. Martin'])
+  // logger.info('Dijsktra: ', sumOfLikesByAuthor['Edsger W. Dijkstra'])
+  const mostLikedAuthor = _.maxBy(_.keys(sumOfLikesByAuthor), (author) => sumOfLikesByAuthor[author])
+  // logger.info('mostLikedAuthor', mostLikedAuthor)
+  // logger.info(sumOfLikesByAuthor[mostLikedAuthor])
+  const result = {
+    'author': mostLikedAuthor,
+    'likes': sumOfLikesByAuthor[mostLikedAuthor]
+  }
+  // logger.info(result)
+  return result
+}
+
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
