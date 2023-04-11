6d883eb5261f8f6aa04b386549958963b2383b49
miahro <mikko.ahro@gmail.com>
2023-04-11 12:48:12 +0300

    replaced then-catch with express-async-errors

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,34 +2,39 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 
-// blogsRouter.get('/', async (request, response, next) => {
-//   const blogs = Blog.find({})
-//   response.json(blogs)
-//     // .then(blogs => {
-//     //   response.json(blogs)
-//     //   //logger.info(blogs[0])
-//     // })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+
+
+
+// blogsRouter.get('/', (request, response, next) => {
+//   Blog.find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//       //logger.info(blogs[0])
+//     })
 //     .catch(error => next(error))
 // })
 
-
-blogsRouter.get('/', (request, response, next) => {
-  Blog.find({})
-    .then(blogs => {
-      response.json(blogs)
-      //logger.info(blogs[0])
-    })
-    .catch(error => next(error))
-})
-
-blogsRouter.post('/', (request, response, next) => {
-  console.log('POST: ',request.path, request.body)
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-  blog.save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
+
+
+
+// blogsRouter.post('/', (request, response, next) => {
+//   console.log('POST: ',request.path, request.body)
+//   const blog = new Blog(request.body)
+//   blog.save()
+//     .then(result => {
+//       response.status(201).json(result)
+//     })
+//     .catch(error => next(error))
+// })
 
 module.exports = blogsRouter
